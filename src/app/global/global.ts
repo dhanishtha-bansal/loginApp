ebecafbf7b0732e31354de0130e774616b8e3725
Dhanishtha Bansal <dhanishtha.bansal@synapse.com>
2018-09-14 17:09:08 +0530

    logged status captured

--- src/app/global/global.ts
+++ src/app/global/global.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class AppGlobals {
     readonly baseAppUrl: string = 'http://localhost:4201/';
+    private logStatusSource = new BehaviorSubject(false);
+    logStatus = this.logStatusSource.asObservable();
     isLoggedIn: boolean = false;
 }
